39ff47b2b4df540d2bd20e8ebec8cd97eff187a0
Neeth Kunnath <neeth.xaver@gmail.com>
2020-06-19 10:47:56 -0400

    first attempt at restructuring

--- src/fmnist.py
+++ src/fmnist.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 import torch
@@ -9,63 +8,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import model.simple as simple
+import utils.helpers as helpers
+
 from torch.utils.tensorboard import SummaryWriter
 
-from model import Net
-
-def matplotlib_imshow(img, ax, one_channel=False):
-    if ax is None:
-        ax = plt.figure().gca()
-    if one_channel:
-        img = img.mean(dim=0)
-    img = img/2 + 0.5 # unnormalize
-    npimg = img.numpy()
-
-    if one_channel:
-        ax.imshow(npimg, cmap='Greys')
-    else:
-        ax.imshow(np.transpose(npimg, (1, 2, 0)))
-
-def images_to_probs(net, images):
-    '''
-    Generates predictions and corresponding probabilities from a trained network and a list of
-    images'
-    '''
-    output = net(images)
-
-    # convert optput probabilities to a predicted class
-    _, preds_tensor = torch.max(output, 1)
-    preds = np.squeeze(preds_tensor.numpy())
-    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
-
-def plot_classes_preds(net, images, labels, classes):
-    '''
-    Generates matplotlib Figure using a trained network, along with images, and lables from a
-    batch, that shows the network's top prediction along with its probability, alongside the
-    actual label, coloring this infomration based on wheterh the prediction was correct or not.
-    Uses the "images_to_probs" function.
-    '''
-
-    preds, probs = images_to_probs(net, images)
-    fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(12, 48), squeeze=False)
-    for idx in np.arange(4):
-        matplotlib_imshow(images[idx], ax=ax[0, idx], one_channel=True)
-        ax[0, idx].set_title('{0}, {1:.1f}%\nlabel: {2}'.format(
-            classes[preds[idx]],
-            probs[idx]*100.0,
-            classes[labels[idx]]),
-            color=('green' if preds[idx]==labels[idx].item() else 'red'))
-    return fig
-
-def select_n_random(data, labels, n=100):
-    '''
-    Selects n random datapoints  and their corresponding labels from a dataset
-    '''
-
-    assert len(data) == len(labels)
-
-    perm = torch.randperm(len(data))
-    return data[perm][:n], labels[perm][:n]
 
 def train(trainset, classes, writer, criterion, optimizer, net):
     # dataloaders
@@ -79,14 +26,14 @@ def train(trainset, classes, writer, criterion, optimizer, net):
     img_grid = torchvision.utils.make_grid(images)
 #
 
-    matplotlib_imshow(img_grid, ax=None, one_channel=True)
+    helpers.matplotlib_imshow(img_grid, ax=None, one_channel=True)
 
     writer.add_image('four_fashion_mnist_images', img_grid)
     writer.add_graph(net, images)
     writer.close()
 
     # select random images and their target indices
-    images, labels = select_n_random(trainset.data, trainset.targets)
+    images, labels = helpers.select_n_random(trainset.data, trainset.targets)
     # get the class labels for each image
     class_labels = [classes[lab] for lab in labels]
 
@@ -121,7 +68,7 @@ def train(trainset, classes, writer, criterion, optimizer, net):
 
                 # ...log a Matplotlib Figure showing the model's predictions on a random mini-batch
                 writer.add_figure('predictions vs. actuals',
-                                  plot_classes_preds(net, inputs, labels, classes),
+                                  helpers.plot_classes_preds(net, inputs, labels, classes),
                                                      global_step=epoch*len(trainloader) + i)
                 running_loss = 0.0
 
@@ -189,7 +136,7 @@ def run():
               'Ankle Boot']
 
 
-    net = Net()
+    net = simple.Net()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
